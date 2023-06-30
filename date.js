3d9782f4650852e6c8c690609781e750082f222d
Sayan Sengupta <66245030+sayan2001ssg@users.noreply.github.com>
2023-06-30 20:52:50 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,5 +1,4 @@
 var today = new Date();
-var options = { day: '2-digit', month: 'short', year: 'numeric' };
 
 var optionsDay = { day: '2-digit' };
 var twoDigitDay = today.toLocaleDateString('en-US', optionsDay);
@@ -12,7 +11,7 @@ var shortMonth = today.toLocaleDateString('en-US', optionsMonth);
 var optionsYear = { year: 'numeric' };
 var year = today.toLocaleDateString('en-US', optionsYear);
 
-var time = "09:20 AM";
+var time = "06:05 AM";
 var vfrom = "12:00 AM";
 var vtill = "11:59 PM";
 console.log(twoDigitDay+' '+shortMonth+' '+year+', '+time);
@@ -21,3 +20,18 @@ console.log(twoDigitDay+' '+shortMonth+' '+year+', '+time);
 document.getElementById('date1').textContent = twoDigitDay+' '+shortMonth+' '+year+', '+time;
 document.getElementById('date2').textContent = twoDigitDay+' '+shortMonth+' '+year+', '+vfrom;
 document.getElementById('date3').textContent = twoDigitDay+' '+shortMonth+' '+year+', '+vtill;
+
+var images = ["qrcode.png", "qrcode2.png", "qrcode3.png", "qrcode4.png", "qrcode5.png"];
+var currentIndex = 0;
+        
+function changeImage() {
+    var img = document.getElementById("image");
+    img.src = images[currentIndex];
+    
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0; // Reset to the first image
+    }
+    
+    setTimeout(changeImage, 1000); // Change image every 1 second
+}
